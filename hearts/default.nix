de7d86a42e7b25c0505ade7f3646420a655a5b2a
Mike Sperber <sperber@deinprogramm.de>
2019-06-13 09:48:59 +0200

    Add cond to Nix dependencies

--- hearts/default.nix
+++ hearts/default.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, mtl, random, stdenv, transformers, free }:
+{ mkDerivation, base, mtl, random, stdenv, transformers, free, cond }:
 mkDerivation {
   pname = "hearts";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base mtl random transformers free ];
+  executableHaskellDepends = [ base mtl random transformers free cond ];
   homepage = "https://github.com/peterthiemann/hearts#readme";
   license = stdenv.lib.licenses.bsd3;
 }
